a21de5b81095bbf5e89e2c1cf6865d5a566838b0
motomotoyoshi <motomoto.git@gmail.com>
2020-06-21 04:09:43 +0900

    update, post update

--- myblog/app/Http/Controllers/PostsController.php
+++ myblog/app/Http/Controllers/PostsController.php
@@ -42,4 +42,22 @@ class PostsController extends Controller
         $post->save();
         return redirect('/');
     }
+
+
+    public function edit(Post $post) {
+        return view('posts.edit')->with('post', $post);
+    }
+
+
+    public function update(Request $request, Post $post)
+    {
+        $this->validate($request, [
+            'title' => 'required|min:3',
+            'body' => 'required'
+        ]);
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return redirect('/');
+    }
 }
